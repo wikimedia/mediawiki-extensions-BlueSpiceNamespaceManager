28a94f69151101750e2361447ce7cbca40993307
Dejan Savuljesku <d.savuljesku@gmail.com>
2018-11-21 15:32:08 +0100

    Upgrade BSPrivacy handler
    Change-Id: I00fe1931e1d80a0c9609847638bec9e5fd9989c3

--- src/Privacy/Handler.php
+++ src/Privacy/Handler.php
@@ -5,22 +5,35 @@ namespace BlueSpice\NamespaceManager\Privacy;
 use BlueSpice\Privacy\IPrivacyHandler;
 
 class Handler implements IPrivacyHandler {
-	protected $user;
 	protected $db;
 
-	public function __construct( \User $user, \Database $db ) {
-		$this->user = $user;
+	public function __construct( \Database $db ) {
 		$this->db = $db;
 	}
 
-	public function anonymize( $newUsername ) {
+	public function anonymize( $oldUsername, $newUsername ) {
 		$this->db->update(
 			'bs_namespacemanager_backup_revision',
 			[ 'rev_user_text' => $newUsername ],
-			[ 'rev_user_text' => $this->user->getName() ]
+			[ 'rev_user_text' => $oldUsername ]
 		);
 
 		return \Status::newGood();
+	}
 
+	public function delete( \User $userToDelete, \User $deletedUser ) {
+		$this->anonymize( $userToDelete->getName(), $deletedUser->getName() );
+
+		$this->db->update(
+			'bs_namespacemanager_backup_revision',
+			[ 'rev_user' => $deletedUser->getId() ],
+			[ 'rev_user' => $userToDelete->getId() ]
+		);
+
+		return \Status::newGood();
+	}
+
+	public function exportData( array $types, $format, \User $user ) {
+		return \Status::newGood( [] );
 	}
 }
