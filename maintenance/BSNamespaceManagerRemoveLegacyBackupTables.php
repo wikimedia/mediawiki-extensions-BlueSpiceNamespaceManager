b2809c8360cce861d3dc90b2c0c729aeeb1de5fc
pwirth <wirth@hallowelt.biz>
2020-06-12 16:26:47 +0200

    Remove legacy backup tables and its privacy handler
    this feature was removed in I65c5a3ec7440563a3373fa1f572b33f312befa50
    
    ERM:19710
    [3.2]
    
    Change-Id: If244942172b592a0dbb4f07279c6c159d4faa3c4

---
+++ maintenance/BSNamespaceManagerRemoveLegacyBackupTables.php
@@ -0,0 +1,46 @@
+<?php
+
+$IP = dirname( dirname( dirname( __DIR__ ) ) );
+require_once "$IP/maintenance/Maintenance.php";
+
+class BSNamespaceManagerRemoveLegacyBackupTables extends LoggedUpdateMaintenance {
+
+	private $tableNames = [
+		'bs_namespacemanager_backup_page',
+		'bs_namespacemanager_backup_revision',
+		'bs_namespacemanager_backup_text',
+	];
+
+	/**
+	 *
+	 * @return true
+	 */
+	protected function doDBUpdates() {
+		$db = $this->getDB( DB_MASTER );
+		foreach ( $this->tableNames as $table ) {
+			$this->output( "\n * delete $table... " );
+			if ( !$db->tableExists( $table ) ) {
+				$this->output( "does not exist => OK" );
+				continue;
+			}
+			$this->output( "exist, deleting ... " );
+			$res = $db->dropTable( $table, __METHOD__ );
+			if ( !$res ) {
+				$this->output( "FAILED" );
+				continue;
+			}
+			$this->output( "OK" );
+		}
+		$this->output( "\n\n" );
+		return true;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	protected function getUpdateKey() {
+		return 'bs_namespacemanager-removelegacybackuptables';
+	}
+
+}
