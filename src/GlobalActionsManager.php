c956a23c0d884e774974333cbddfa2811f6fcbb6
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-04 12:48:37 +0200

    Add extension to SkinSlot GlobalActionsManager
    [4.x]
    
    Change-Id: I4ee482fc737617d78238586ba92fd553d248123f

---
+++ src/GlobalActionsManager.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace BlueSpice\NamespaceManager;
+
+use Message;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use SpecialPage;
+
+class GlobalActionsManager extends RestrictedTextLink {
+
+	/**
+	 *
+	 */
+	public function __construct() {
+		parent::__construct( [] );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId(): string {
+		return 'ga-bs-namespacemanager';
+	}
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPermissions(): array {
+		$permissions = [
+			'namespacemanager-viewspecialpage'
+		];
+		return $permissions;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHref(): string {
+		$tool = SpecialPage::getTitleFor( 'NamespaceManager' );
+		return $tool->getLocalURL();
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return Message::newFromKey( 'bs-namespacemanager-label' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		return Message::newFromKey( 'bs-namespacemanager-desc' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getAriaLabel(): Message {
+		return Message::newFromKey( 'bs-namespacemanager-label' );
+	}
+}
