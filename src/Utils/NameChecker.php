58a6652f699102f1958c7d8a0c42e4f3649f57bf
Markus Glaser <glaser@hallowelt.com>
2022-02-18 09:30:37 +0100

    Fix namespace creation with empty alias
    In the wake of this, factured out the check logic and added unit tests
    for it. Also, used msg() instead of wfMessage()
    
    ERM#18750
    
    [4.0.x]
    
    Change-Id: I9606b6910393251b7c6338d608052807655dc884

---
+++ src/Utils/NameChecker.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace BlueSpice\NamespaceManager\Utils;
+
+use BlueSpice\Api\Response\Standard;
+use Database;
+use MessageLocalizer;
+
+class NameChecker {
+
+	/**
+	 *
+	 * @var array
+	 */
+	private $namespaceNames = [];
+
+	/**
+	 *
+	 * @var array
+	 */
+	private $namespaceAliases = [];
+
+	/**
+	 *
+	 * @var Database
+	 */
+	private $db = null;
+
+	/**
+	 *
+	 * @var MessageLocalizer
+	 */
+	private $messageLocalizer = null;
+
+	/**
+	 *
+	 * @param array $namespaceNames
+	 * @param array $namespaceAliases
+	 * @param Database|null $db
+	 * @param MessageLocalizer $messageLocalizer
+	 */
+	public function __construct(
+		$namespaceNames,
+		$namespaceAliases,
+		?Database $db,
+		MessageLocalizer $messageLocalizer
+	) {
+		$this->namespaceNames = $namespaceNames;
+		$this->namespaceAliases = $namespaceAliases;
+		$this->db = $db;
+		$this->messageLocalizer = $messageLocalizer;
+	}
+
+	/**
+	 * Checks if a namespace or alias name passes the naming conventions
+	 * @param string $namespaceName
+	 * @param string $namespaceAlias
+	 * @return Standard
+	 */
+	public function checkNamingConvention( $namespaceName, $namespaceAlias ) {
+		$oResult = new Standard();
+		if ( strlen( $namespaceName ) < 2 ) {
+			$oResult->message = $this->messageLocalizer->msg( 'bs-namespacemanager-ns-length' )->plain();
+			return $oResult;
+		}
+
+		if ( !empty( $namespaceAlias ) && strlen( $namespaceAlias ) < 2 ) {
+			$oResult->message = $this->messageLocalizer->msg( 'bs-namespacemanager-ns-length' )->plain();
+			return $oResult;
+		}
+
+		if (
+			!preg_match( '%^[a-zA-Z_\\x80-\\xFF][a-zA-Z0-9_\\x80-\\xFF]{1,99}$%i', $namespaceName )
+			) {
+			$oResult->message = $this->messageLocalizer->msg( 'bs-namespacemanager-wrong-name' )->plain();
+			return $oResult;
+		}
+
+		if ( !empty( $namespaceAlias )
+			&& !preg_match( '%^[a-zA-Z_\\x80-\\xFF][a-zA-Z0-9_\\x80-\\xFF]{1,99}$%i', $namespaceAlias ) ) {
+			$oResult->message = $this->messageLocalizer->msg( 'bs-namespacemanager-wrong-alias' )->plain();
+			return $oResult;
+		}
+		$oResult->success = true;
+		return $oResult;
+	}
+
+	/**
+	 * Checks it a namespace name or alias is already in use
+	 * @param string $namespaceName
+	 * @param string $namespaceAlias
+	 * @param int $namespaceId
+	 * @return Standard
+	 */
+	public function checkExists( $namespaceName, $namespaceAlias, $namespaceId ) {
+		$oResult = new Standard();
+		foreach ( $this->namespaceNames as $sKey => $sNamespaceFromArray ) {
+			if ( strtolower( $sNamespaceFromArray ) == strtolower( $namespaceName ) ) {
+				if ( $sKey === $namespaceId ) {
+					continue;
+				}
+				$oResult->message = $this->messageLocalizer->msg( 'bs-namespacemanager-ns-exists' )->plain();
+				return $oResult;
+			}
+		}
+		if ( !empty( $namespaceAlias ) ) {
+			foreach ( $this->namespaceNames as $sKey => $sNamespaceFromArray ) {
+				if ( strtolower( $sNamespaceFromArray ) == strtolower( $namespaceAlias ) ) {
+					$oResult->message = $this->messageLocalizer->msg(
+						'bs-namespacemanager-alias-exists-as-ns'
+					)->plain();
+					return $oResult;
+				}
+			}
+
+			if ( $this->isAliasInUse( $namespaceId, $namespaceAlias ) ) {
+				$nsName = $this->namespaceNames[$this->namespaceAliases[$namespaceAlias]];
+				$oResult->message = $this->messageLocalizer->msg(
+					'bs-namespacemanager-alias-exists', $nsName
+				)->plain();
+				return $oResult;
+			}
+
+		}
+		$oResult->success = true;
+		return $oResult;
+	}
+
+	/**
+	 *
+	 * @param int $namespaceId
+	 * @param string $namespaceAlias
+	 * @return bool
+	 */
+	protected function isAliasInUse( $namespaceId, $namespaceAlias ) {
+		if ( empty( $namespaceAlias ) || !isset( $this->namespaceAliases[$namespaceAlias] ) ) {
+			return false;
+		}
+		if ( $this->namespaceAliases[$namespaceAlias] === $namespaceId ) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Checks if there is a title in Main namespace which begins with
+	 * the proposed namespace name
+	 * @param string $namespaceName
+	 * @param string $namespaceAlias
+	 * @return bool
+	 */
+	public function checkPseudoNamespace( $namespaceName, $namespaceAlias ) {
+		$oResult = new Standard();
+		$res = $this->db->select(
+			[ 'page' ],
+			'page_title',
+			[
+				'page_namespace' => NS_MAIN,
+				'page_title LIKE "%:%"'
+			]
+		);
+
+		$titlesInMainBeginWithNamespaceNameOrAlias = [];
+
+		foreach ( $res as $row ) {
+			if ( strpos( strtolower( $row->page_title ), strtolower( $namespaceName . ":" ) ) === 0 ) {
+				$titlesInMainBeginWithNamespaceNameOrAlias[] = $row->page_title;
+			}
+			if ( !empty( $namespaceAlias )
+				&& strpos( strtolower( $row->page_title ), strtolower( $namespaceAlias . ":" ) ) === 0
+			) {
+				$titlesInMainBeginWithNamespaceNameOrAlias[] = $row->page_title;
+			}
+		}
+
+		if ( count( $titlesInMainBeginWithNamespaceNameOrAlias ) > 0 ) {
+			$oResult->message = $this->messageLocalizer->msg( 'bs-namespacemanager-pseudo-ns' )->plain();
+			return $oResult;
+		}
+
+		$oResult->success = true;
+		return $oResult;
+	}
+}
---
+++ tests/phpunit/Utils/NameCheckerTest.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace BlueSpice\NamespaceManager\Tests;
+
+use BlueSpice\NamespaceManager\Utils\NameChecker;
+use Database;
+use MediaWikiIntegrationTestCase;
+use MessageLocalizer;
+
+/**
+ * @covers \BlueSpice\NamespaceManager\Utils\NameChecker
+ */
+class NameCheckerTest extends MediaWikiIntegrationTestCase {
+
+	private $namespaceList = [
+			-2 => 'Media',
+			-1 => 'Special',
+			0  => 'Main',
+			1 => 'Talk',
+			3000 => 'Exists',
+			3001 => 'Exists_Talk',
+			3002 => 'SomeNS',
+			3003 => 'SomeNS_Talk'
+		];
+
+	private $namespaceAliasList = [
+			'ExistsReally' => 3000,
+			'ExistsReally_talk' => 3001,
+			'ExistsElsewhere' => 3002,
+			'ExistsElsewhere_talk' => 3003
+		];
+
+	/**
+	 * @covers \BlueSpice\NamespaceManager\Utils\NameChecker::checkNamingConvention()
+	 * @dataProvider checkNameDataProvider()
+	 */
+	public function testCheckNamingConvention( $name, $alias, $expected, $expectedMessage ) {
+		$messageLocalizer = $this->createMock( MessageLocalizer::class );
+		$messageLocalizer
+			->method( 'msg' )
+			->will( $this->returnCallback( static function ( $msgKey ) {
+					return new \RawMessage( $msgKey );
+			} ) );
+
+		$nameChecker = new NameChecker( $this->namespaceList, $this->namespaceAliasList, null, $messageLocalizer );
+		$actual = $nameChecker->checkNamingConvention( $name, $alias );
+
+		$this->assertEquals( $expected, $actual->success );
+		$this->assertEquals( $expectedMessage, $actual->message );
+	}
+
+	/**
+	 * @covers \BlueSpice\NamespaceManager\Utils\NameChecker::checkExists()
+	 * @dataProvider checkExistsDataProvider()
+	 */
+	public function testCheckExists( $name, $alias, $id, $expected, $expectedMessage ) {
+		$messageLocalizer = $this->createMock( MessageLocalizer::class );
+		$messageLocalizer
+			->method( 'msg' )
+			->will( $this->returnCallback( static function ( $msgKey ) {
+					return new \RawMessage( $msgKey );
+			} ) );
+
+		$nameChecker = new NameChecker( $this->namespaceList, $this->namespaceAliasList, null, $messageLocalizer );
+		$actual = $nameChecker->checkExists( $name, $alias, $id );
+
+		$this->assertEquals( $expected, $actual->success );
+		$this->assertEquals( $expectedMessage, $actual->message );
+	}
+
+	/**
+	 * @covers \BlueSpice\NamespaceManager\Utils\NameChecker::checkPseudoNamespace()
+	 * @dataProvider checkPseudoNamespaceDataProvider()
+	 */
+	public function testCheckPseudoNamespace( $name, $alias, $expected, $expectedMessage ) {
+		$mockDBData = [
+			(object)[ 'page_title' => 'Normal:Page' ],
+			(object)[ 'page_title' => 'Pseudo:Page' ],
+		];
+		$db = $this->createMock( Database::class );
+		$db->method( 'select' )->willReturn( $mockDBData );
+
+		$messageLocalizer = $this->createMock( MessageLocalizer::class );
+		$messageLocalizer
+			->method( 'msg' )
+			->will( $this->returnCallback( static function ( $msgKey ) {
+					return new \RawMessage( $msgKey );
+			} ) );
+
+		$nameChecker = new NameChecker( $this->namespaceList, $this->namespaceAliasList, $db, $messageLocalizer );
+		$actual = $nameChecker->checkPseudoNamespace( $name, $alias );
+
+		$this->assertEquals( $expected, $actual->success );
+		$this->assertEquals( $expectedMessage, $actual->message );
+	}
+
+	public function checkNameDataProvider() {
+		return [
+			[ "Test", "Toast", true, null ],
+			[ "Test", "", true, null ],
+			[ "Täst", "", true, null ],
+			[ "Te_st", "", true, null ],
+			[ "Test123", "", true, null ],
+			[ "T", "Test", false, 'bs-namespacemanager-ns-length' ],
+			[ "Test", "T", false, 'bs-namespacemanager-ns-length' ],
+			[ "Te-st", "", false, 'bs-namespacemanager-wrong-name' ],
+			[ "Te st", "", false, 'bs-namespacemanager-wrong-name' ],
+			[ "123Test", "", false, 'bs-namespacemanager-wrong-name' ],
+			[ "Test", "Te-st", false, 'bs-namespacemanager-wrong-alias' ],
+			[ "Test", "Te st", false, 'bs-namespacemanager-wrong-alias' ],
+			[ "Test", "123Test", false, 'bs-namespacemanager-wrong-alias' ],
+		];
+	}
+
+	public function checkExistsDataProvider() {
+		return [
+			// These are the cases for adding new namespaces
+			[ "Test", "Toast", 3200, true, null ],
+			[ "Test", "Test", 3200, true, null ],
+			[ "Test", "", 3200, true, null ],
+			[ "Exists", "Test", 3200, false, 'bs-namespacemanager-ns-exists' ],
+			[ "Test", "Exists", 3200, false, 'bs-namespacemanager-alias-exists-as-ns' ],
+			[ "Exists", "Exists", 3200, false, 'bs-namespacemanager-ns-exists' ],
+			[ "Test", "ExistsReally", 3200, false, 'bs-namespacemanager-alias-exists' ],
+			// These are the cases for editing namespaces
+			// No change in name and alias
+			[ "Exists", "ExistsReally", 3000, true, null ],
+			 // Name changes
+			[ "Test", "ExistsReally", 3000, true, null ],
+			 // Alias changes
+			[ "Exists", "Test", 3000, true, null ],
+			[ "SomeNS", "ExistsReally", 3000, false, 'bs-namespacemanager-ns-exists' ],
+			[ "Exists", "SomeNS", 3000, false, 'bs-namespacemanager-alias-exists-as-ns' ],
+			[ "Test", "ExistsElsewhere", 3000, false, 'bs-namespacemanager-alias-exists' ],
+		];
+	}
+
+	public function checkPseudoNamespaceDataProvider() {
+		return [
+			// Namespace name
+			// totaly unrelated
+			[ "Fun", "Realfun", true, null ],
+			 // using "page" component but not namespace
+			[ "Page", "Realtest", true, null ],
+			 // using part of the pseudo namespace
+			[ "Pseu", "Realtest", true, null ],
+			 // using more than the pseudo namespace
+			[ "Pseudofun", "Realtest", true, null ],
+			[ "Pseudo", "Realtest", false, 'bs-namespacemanager-pseudo-ns' ],
+			// Alias name
+			[ "Test", "Fun", true, null ],
+			[ "Test", "Page", true, null ],
+			[ "Test", "Pseu", true, null ],
+			[ "Test", "Pseudofun", true, null ],
+			[ "Test", "Pseudo", false, 'bs-namespacemanager-pseudo-ns' ]
+		];
+	}
+
+}
